f902bdda0ea8388e3563ac3bfa18a4cdedf5038c
Majkels92 <michal.skowr92@gmail.com>
2023-10-01 21:12:48 +0200

    Adds todolist app, configure urls and adds html files

--- todolist/urls.py
+++ todolist/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('todolist.urls'))
+    path('', views.index, name="homepage"),
+    path('todolist', views.todolist, name="task-list"),
+    path('todolist/<slug:slug>', name="single-task")
 ]
