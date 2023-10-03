e630dc176c9cb3eb157b567d45b7881fd79bb0e6
Majkels92 <michal.skowr92@gmail.com>
2023-10-03 20:44:22 +0200

    Add to model is_done field, register models and define displayed content

--- todolist/admin.py
+++ todolist/admin.py
@@ -3,9 +3,14 @@ from .models import User, Task
 # Register your models here.
 
 
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ["name", "surname", "email", "nickname"]
 
 
-admin.site.register(User, UserAdmin)
-admin.site.register(Task)
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["title", "deadline", "user", "slug", "description"]
+    exclude = ["is_done"]
+    prepopulated_fields = {"slug": ("title",)}
+
--- todolist/models.py
+++ todolist/models.py
@@ -7,8 +7,12 @@ class User(models.Model):
     email = models.EmailField()
     nickname = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
 
 class Task(models.Model):
+    is_done = models.BooleanField(default=False)
     title = models.CharField(max_length=100)
     add_time = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField()
