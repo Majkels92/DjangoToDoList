3645f89390a36ee736452d62757b8fd6d5504d25
Majkels92 <michal.skowr92@gmail.com>
2023-10-12 19:59:18 +0200

    add styles and size of form

--- todolist/forms.py
+++ todolist/forms.py
@@ -6,3 +6,9 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         exclude = ["add_time", "is_done"]
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'enter title here...'}),
+            'deadline': forms.TextInput(attrs={'placeholder': 'd.m.y h:m'}),
+            'description': forms.Textarea(attrs={'placeholder': 'enter description here...',
+                                                 'rows': '8'}),
+        }
