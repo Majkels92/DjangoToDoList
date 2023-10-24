30016d056d2b1a20f0be22ab640e26478e7837b7
Majkels92 <michal.skowr92@gmail.com>
2023-10-24 21:13:29 +0200

    change settings.py

--- djangoToDoList/settings.py
+++ djangoToDoList/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 from decouple import config
 from pathlib import Path
 from os import getenv
@@ -26,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = getenv("IS_PRODUCTION", True)
 
 ALLOWED_HOSTS = [
-    getenv("APP_HOST")
+    os.getenv('ALLOWED_HOSTS').split(' ')
 ]
 
 
