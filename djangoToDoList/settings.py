06360d8f590ff1a9d6329b2fb1caff72f9a7f5c0
Majkels92 <michal.skowr92@gmail.com>
2023-10-25 19:34:38 +0200

    change settings.py

--- djangoToDoList/settings.py
+++ djangoToDoList/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from decouple import config
 from pathlib import Path
 from os import getenv
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
