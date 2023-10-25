b970eae41d2ff8dcc77a55475b68e5ab102c00b4
Majkels92 <michal.skowr92@gmail.com>
2023-10-25 20:53:01 +0200

    change settings.py

--- djangoToDoList/settings.py
+++ djangoToDoList/settings.py
@@ -24,7 +24,7 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = getenv("IS_PRODUCTION", True)
+DEBUG = getenv("IS_PRODUCTION", False)
 
 ALLOWED_HOSTS = ['ms-todolist.onrender.com', 'localhost', '127.0.0.1']
 
@@ -77,13 +77,19 @@ WSGI_APPLICATION = 'djangoToDoList.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("postgres://ms_todolistapp_db_user:lUvIleOJ5wcA891ElzTOu76j4TJFUcTq@dpg-ckskvog5vl2c73a9btug-a.frankfurt-postgres.render.com/ms_todolistapp_db"))
-}
-    #     {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+
+
+if not DEBUG:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get("postgres://ms_todolistapp_db_user:lUvIleOJ5wcA891ElzTOu76j4TJFUcTq@dpg-ckskvog5vl2c73a9btug-a.frankfurt-postgres.render.com/ms_todolistapp_db"))}
+else:
+    DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': 'db.sqlite3',
+                }
+    }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
