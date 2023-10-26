b738e8a62519d2211ee58892fbee3e0f62dc120c
Majkels92 <michal.skowr92@gmail.com>
2023-10-26 19:39:00 +0200

    change settings.py

--- djangoToDoList/settings.py
+++ djangoToDoList/settings.py
@@ -12,6 +12,7 @@ from pathlib import Path
 from os import getenv, environ
 import dj_database_url
 from dotenv import load_dotenv
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,11 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = getenv("IS_PRODUCTION", False)
+# DEBUG = getenv("IS_PRODUCTION", False)
+DEBUG = True
 
 ALLOWED_HOSTS = ['ms-todolist.onrender.com', 'localhost', '127.0.0.1']
 
